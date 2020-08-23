76e1b4bec2fee556729c9dbdc49efbf47877942e
anna9grace <anna9grace@gmail.com>
2020-08-23 14:10:09 +0300

    исправляет замечания проверяющего наставника

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,26 @@
 var menuToggle = document.querySelector(".page-header__menu");
 var menu = document.querySelector(".main-nav");
-
 var mobile = window.matchMedia("(max-width: 767px)");
 
 if (mobile.matches) {
   menu.classList.add("hidden");
   menuToggle.classList.remove("hidden");
 }
+
+var hideMenu = function(mq) {
+  if (mq.matches) {
+    menuToggle.classList.remove("hidden");
+    if (menuToggle.classList.contains("page-header__menu--open")) {
+      menu.classList.add("hidden");
+    }
+  } else {
+    menu.classList.remove("hidden");
+    menuToggle.classList.add("hidden");
+  }
+}
+
+mobile.addListener(hideMenu);
+
 
 menuToggle.addEventListener("click", function(evt) {
     evt.preventDefault();
